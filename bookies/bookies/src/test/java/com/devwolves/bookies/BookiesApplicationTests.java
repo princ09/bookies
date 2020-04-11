52c8a7bff39a09a06b4cb66d4406a4136992745f
Rajkumar Gupta <Rajkumar Gupta@DESKTOP-ODO76FG>
2020-04-11 14:54:27 +0530

    Backend Files Added And Application.properties file Updated. Intial Comment

---
+++ bookies/bookies/src/test/java/com/devwolves/bookies/BookiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devwolves.bookies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
